719eaf90e5e31ec14ce9e3a7b515c832bd1d5287
Maverick <tgmaverickgt@gmail.com>
2025-10-04 15:06:09 +0200

    Add selecting config options from database

--- classes/Config.php
+++ classes/Config.php
@@ -5,5 +5,16 @@
     {
         public static $RiptideVersion = "1.0.0";
         public static $MinPHPVersion = "8.1";
+        public static $Options;
+
+        public static function Init()
+        {
+            $q = Database::Query("SELECT * FROM `/@table:config`");
+            
+            while($r = $q -> fetch_assoc())
+            {
+                self::$Options[$r["id"]] = $r["value"];
+            }
+        }
     }
 ?>
