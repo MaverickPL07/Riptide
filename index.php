df709eeaaa1739e95c054eefeccdc1d4dbd6e517
Maverick <tgmaverickgt@gmail.com>
2025-10-05 12:38:16 +0200

    Change index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-    require_once __DIR__ . "/classes/Core.php";
+    require_once __DIR__ . "/config/config.php";
+    require_once ROOT . "/classes/Core.php";
+    
     use Riptide\Core;
 
     Core::Run();
